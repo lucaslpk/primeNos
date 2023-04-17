ee2fe7ab9069f83d03fbb7c1daa9af2cff1bf2e2
Lucas Lpk <lukasz.lepkowski@gmail.com>
2023-04-17 21:34:34 +0100

    final

--- is_prime.py
+++ is_prime.py
@@ -18,10 +18,12 @@ def is_prime2(num):             #second version -> each time it checks it reduce
     return True
 
 # both the above functions only check one number if it's prime and in order to find all from range both functions need to be used in loops
-# in order to further streamline the process of finding all primes from a range, this 3rd function is written which will find all from range at once
+#
+# in order to further streamline the process of finding all primes from a range, this 3rd function is written, which will find all primes from range at once and store them in a list
 # thanks to this approach we will be able to greatly limit the amount of operations, as if a number is indivisible by 2 it will never be indivisible by, 4, 6, 8 and any other even number
 # if a number is indivisible by 3 it will never be divisible by 6, 9, 12 and any other number disible by 3 and so on. Therefore we only need to check if a number is divisible by
-# any of the prime number smaller than itself. But it is only feasbiel if a function remembers all previously found prime numbers
+# any of the prime numbers smaller than itself. But it is only feasible if the function remembers all previously found prime numbers
+# are primes(ran) takes range as argument an returns a list of primes from that range
 
 def are_primes(ran) :            
     primes_list = [2] 
@@ -44,7 +46,7 @@ def are_primes(ran) :
 
         i += 1       
 
-    primes_list.insert(0, 1)    # 1 needs to be added to the list of primes at the end, otherwise every number % 1 == 0 
+    primes_list.insert(0, 1)    # 1 needs to be inserted to the list of primes at the end of processing, otherwise every number % 1 == 0 and the above loop will return isprime = False for every number
     return primes_list
 
 def compareAndPrint(ran) :   #compares and outputs all prime numbers found by both functions
@@ -72,7 +74,7 @@ def compareSum(ran) :       #compares sums of all prime numbers found by all fun
             sum2 += i
     sum3 = sum(are_primes(ran))
     if sum2 == sum1 == sum3 :
-        print("OK")
+        print("OK - sums of all prime numbers from range (1 to %d) returned by all 3 functions are equal to one another." %(ran))
     else :
          print("Error")
 
@@ -93,9 +95,11 @@ def compare_time(ran) :      #this function will compare how much time each func
     are_primes(ran)
     print("Function No 3 took %f seconds to find all primes from range 1 to %d" % (time.time() - start_time, ran))
 
-# TESTING: uncomment relevant line depending on what you wnat to test
+# TESTING: uncomment relevant line depending on what you what to test
 #are_primes(5)
 #print(are_primes(10))
 compareAndPrint(200)
 compareSum(10000)
 compare_time(50000)
+
+# strange behaviour - if you run with "Start Debugging" option (F5) time reported by "compare_time" function is a lot more than if you run without debugging
